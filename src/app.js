bf057a3cd852b51831f90ad4517f91115b6b59fb
Diego Melgar <dlopez.project.sv@gmail.com>
2018-07-07 16:12:25 -0600

    :wrench: Removed unnecesary packages

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
-import http from 'http'
-import { env, mongo, port, ip, apiRoot } from './config'
-import mongoose from './services/mongoose'
-import express from './services/express'
-import api from './api'
+import http from "http";
+import { env, port, ip, apiRoot } from "./config";
+import express from "./services/express";
+import api from "./api";
 
-const app = express(apiRoot, api)
-const server = http.createServer(app)
-
-mongoose.connect(mongo.uri)
-mongoose.Promise = Promise
+const app = express(apiRoot, api);
+const server = http.createServer(app);
 
 setImmediate(() => {
   server.listen(port, ip, () => {
-    console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
-  })
-})
+    console.log(
+      "Express server listening on http://%s:%d, in %s mode",
+      ip,
+      port,
+      env
+    );
+  });
+});
 
-export default app
+export default app;
