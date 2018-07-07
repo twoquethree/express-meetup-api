88f547f2ed2dc3328c00c9590eb2d14c1bb82573
Diego Melgar <dlopez.project.sv@gmail.com>
2018-07-07 16:16:50 -0600

    :wrench: Changed API port and IP

--- src/config.js
+++ src/config.js
@@ -1,32 +1,32 @@
 /* eslint-disable no-unused-vars */
-import path from 'path'
-import merge from 'lodash/merge'
+import path from "path";
+import merge from "lodash/merge";
 
 /* istanbul ignore next */
-const requireProcessEnv = (name) => {
+const requireProcessEnv = name => {
   if (!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable')
+    throw new Error("You must set the " + name + " environment variable");
   }
-  return process.env[name]
-}
+  return process.env[name];
+};
 
 /* istanbul ignore next */
-if (process.env.NODE_ENV !== 'production') {
-  const dotenv = require('dotenv-safe')
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv-safe");
   dotenv.load({
-    path: path.join(__dirname, '../.env'),
-    sample: path.join(__dirname, '../.env.example')
-  })
+    path: path.join(__dirname, "../.env"),
+    sample: path.join(__dirname, "../.env.example")
+  });
 }
 
 const config = {
   all: {
-    env: process.env.NODE_ENV || 'development',
-    root: path.join(__dirname, '..'),
-    port: process.env.PORT || 9000,
-    ip: process.env.IP || '0.0.0.0',
-    apiRoot: process.env.API_ROOT || '',
-    masterKey: requireProcessEnv('MASTER_KEY'),
+    env: process.env.NODE_ENV || "development",
+    root: path.join(__dirname, ".."),
+    port: process.env.PORT || 8080,
+    ip: process.env.IP || "127.0.0.1",
+    apiRoot: process.env.API_ROOT || "",
+    masterKey: requireProcessEnv("MASTER_KEY"),
     mongo: {
       options: {
         db: {
@@ -35,10 +35,10 @@ const config = {
       }
     }
   },
-  test: { },
+  test: {},
   development: {
     mongo: {
-      uri: 'mongodb://localhost/horchata-meetup-server-dev',
+      uri: "mongodb://localhost/horchata-meetup-server-dev",
       options: {
         debug: true
       }
@@ -48,10 +48,11 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/horchata-meetup-server'
+      uri:
+        process.env.MONGODB_URI || "mongodb://localhost/horchata-meetup-server"
     }
   }
-}
+};
 
-module.exports = merge(config.all, config[config.all.env])
-export default module.exports
+module.exports = merge(config.all, config[config.all.env]);
+export default module.exports;
